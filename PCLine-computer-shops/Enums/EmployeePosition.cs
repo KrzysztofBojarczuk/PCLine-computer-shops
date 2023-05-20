1eb88f7a3785f0e66dbd6651698867e0b1000696
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2023-05-20 07:03:41 +0200

    adding enum EmployeePosition

---
+++ PCLine-computer-shops/Enums/EmployeePosition.cs
@@ -0,0 +1,11 @@
+﻿namespace PCLine_computer_shops.Enums
+{
+    public enum EmployeePosition
+    {
+        Menager = 1,
+        Seller = 2,
+        Technician = 3,
+        OfficeWorker = 4,
+        Driver = 5
+    }
+}
