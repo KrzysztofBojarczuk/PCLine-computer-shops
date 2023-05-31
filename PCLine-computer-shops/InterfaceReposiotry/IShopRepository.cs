20f3fcfd8bd289735a5851e9d38e94d9bf5fcb5d
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2023-05-31 06:40:29 +0200

    adding method getshop

--- PCLine-computer-shops/InterfaceReposiotry/IShopRepository.cs
+++ PCLine-computer-shops/InterfaceReposiotry/IShopRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace PCLine_computer_shops.InterfaceReposiotry
+﻿using PCLine_computer_shops.Models;
+
+namespace PCLine_computer_shops.InterfaceReposiotry
 {
     public interface IShopRepository
     {
+        Task<ICollection<Shop>> GetAllShops(string searchString);
     }
 }
