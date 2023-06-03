c9cabc94e9d1613749c82372472edfc6725308d3
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2023-06-03 06:59:38 +0200

    interface

--- PCLine-computer-shops/InterfaceReposiotry/IShopRepository.cs
+++ PCLine-computer-shops/InterfaceReposiotry/IShopRepository.cs
@@ -5,5 +5,9 @@ namespace PCLine_computer_shops.InterfaceReposiotry
     public interface IShopRepository
     {
         Task<ICollection<Shop>> GetAllShops(string searchString);
+        Task<Shop> CreateShop(Shop shop);
+        Task<Shop> GetShopById(int id);
+        Task<Shop> UpdateShop(Shop updateShop);
+        Task<Shop> DeleteShop(int id);
     }
 }
