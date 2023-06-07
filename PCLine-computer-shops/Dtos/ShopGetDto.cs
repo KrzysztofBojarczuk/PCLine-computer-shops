c9d62c39879ca7dfc0cc6869713d4bd3af02af4e
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2023-06-07 07:05:06 +0200

    Dto

---
+++ PCLine-computer-shops/Dtos/ShopGetDto.cs
@@ -0,0 +1,11 @@
+﻿using PCLine_computer_shops.Models;
+
+namespace PCLine_computer_shops.Dtos
+{
+    public class ShopGetDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StarthDate { get; set; }
+    }
+}
