71efce02a4a17ceb2226b895f17ba5f79117e046
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2023-07-24 18:16:32 +0200

    shops controoler

---
+++ PCLine-computer-shops/Controllers/ShopsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PCLine_computer_shops.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShopsController : ControllerBase
+    {
+    }
+}
