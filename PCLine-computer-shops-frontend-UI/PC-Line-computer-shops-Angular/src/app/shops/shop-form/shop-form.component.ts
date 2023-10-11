424f6d62d1d5372db7532f95a80aebc3dbb6944d
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2023-10-11 11:51:43 +0200

    adding update form to

--- PCLine-computer-shops-frontend-UI/PC-Line-computer-shops-Angular/src/app/shops/shop-form/shop-form.component.ts
+++ PCLine-computer-shops-frontend-UI/PC-Line-computer-shops-Angular/src/app/shops/shop-form/shop-form.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable } from 'rxjs';
 import { Country } from 'src/app/enums/country';
 import { Shop } from 'src/app/models/shop';
+import { ShopCreate } from 'src/app/models/shop-create';
 import { ShopService } from 'src/app/services/shop.service';
 
 @Component({
@@ -21,20 +23,20 @@ export class ShopFormComponent {
     { value: 3, name: 'France' }
   ];
 
+  title: string = "Add New Shop";
 
-  constructor(private formBuilder: FormBuilder, private shopService: ShopService, public dialogRef: MatDialogRef<ShopFormComponent>) {
+  constructor(private formBuilder: FormBuilder, private shopService: ShopService, public dialogRef: MatDialogRef<ShopFormComponent>, private snackBar: MatSnackBar) {
     this.shopForm = this.formBuilder.group({
       name: ['', Validators.required],
-      startDate: ['', Validators.required],
       country: ['', Validators.required],
+      startDate: ['', Validators.required],
     })
   }
 
-  submit(shop: Shop) {
-    console.log(shop);
-
-    this.shopService.postShop(shop).subscribe();
-    this.dialogRef.close()
+  submit(shop: ShopCreate) {
+    this.shopService.postShop(shop).subscribe();;
+    this.dialogRef.close();
   }
+
 }
 
---
+++ PCLine-computer-shops-frontend-UI/PC-Line-computer-shops-Angular/src/app/shops/shop-update/shop-update.component.ts
@@ -0,0 +1,35 @@
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Shop } from 'src/app/models/shop';
+import { ShopService } from 'src/app/services/shop.service';
+
+@Component({
+  selector: 'app-shop-update',
+  templateUrl: './shop-update.component.html',
+  styleUrls: ['./shop-update.component.scss']
+})
+export class ShopUpdateComponent {
+
+  shopForm: FormGroup;
+  countries: { value: number; name: string }[] = [
+    { value: 1, name: 'Poland' },
+    { value: 2, name: 'Germany' },
+    { value: 3, name: 'France' }
+  ];
+
+  title: string = "Update Shop";
+
+  constructor(private formBuilder: FormBuilder, private shopService: ShopService, public dialogRef: MatDialogRef<ShopUpdateComponent>, @Inject(MAT_DIALOG_DATA) public shop: Shop) {
+    this.shopForm = this.formBuilder.group({
+      name: [shop.name, Validators.required],
+      country: [shop.country, Validators.required],
+      startDate: [shop.startDate, Validators.required],
+    });
+  }
+
+  update(shop: Shop) {
+    this.shopService.updateShop(this.shop.shopId, shop).subscribe();
+    this.dialogRef.close();
+  }
+}
