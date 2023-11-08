afd1e1cb046ee010f8af91d7b1a67f718f643a7f
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2023-11-08 06:59:06 +0100

    adding task model

---
+++ PCLine-computer-shops/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using PCLine_computer_shops.Enums;
+
+namespace PCLine_computer_shops.Models
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public DateTime TaskCreatedDate { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int TimeEstiamted { get; set; }
+        public StatusTask TaskStatus { get; set; }
+        public Address? Address { get; set; }
+        public Employee? Employee { get; set; }
+    }
+}
