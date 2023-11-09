96c6dc17a1653e37ef882a231d51d39056020d8d
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2023-11-09 08:08:19 +0100

    adding dto

---
+++ PCLine-computer-shops/Dtos/TaskEmployeeGetDto.cs
@@ -0,0 +1,14 @@
+﻿using PCLine_computer_shops.Enums;
+
+namespace PCLine_computer_shops.Dtos
+{
+    public class TaskEmployeeGetDto
+    {
+        public int TaskId { get; set; }
+        public DateTime TaskCreatedDate { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int TimeEstiamted { get; set; }
+        public StatusTask TaskStatus { get; set; }
+    }
+}
