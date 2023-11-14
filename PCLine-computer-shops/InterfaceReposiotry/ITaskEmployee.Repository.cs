1e2eb989aaef08fc1b92b892b503e759e9b13170
KrzysztofBojarczuk <krzysztofbojarczuk@wp.pl>
2023-11-14 07:13:18 +0100

    adding interface

---
+++ PCLine-computer-shops/InterfaceReposiotry/ITaskEmployee.Repository.cs
@@ -0,0 +1,14 @@
+﻿using PCLine_computer_shops.Enums;
+using PCLine_computer_shops.Models;
+
+namespace PCLine_computer_shops.InterfaceReposiotry
+{
+    public interface ITaskEmployee
+    {
+        Task<ICollection<TaskEmployee>> GetAllTaskEmployees(string searchTerm);
+        Task<TaskEmployee> CreateTaskEmployee(TaskEmployee taskEmployee);
+        Task<TaskEmployee> GetTaskEmployeeById(int taskEmployeeId);
+        Task<TaskEmployee> UpdateTaskEmployee(TaskEmployee updateTaskEmployee);
+        Task<TaskEmployee> DeleteTaskEmployee(int taskEmployeeId);
+    }
+}
