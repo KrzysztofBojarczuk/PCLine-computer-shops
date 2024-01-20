b9e78b45e7345635d23fb0b04f273bdca536110a
Krzysztof Bojarczuk <krzysztofbojarczuk@wp.pl>
2024-01-20 12:33:11 +0100

    adding vue

---
+++ PCLine-computer-shops-frontend-UI/pc-line-computer-shops-vue/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>{{ name }}</div>
+    <div v-text="greetings"></div>
+    <v-chip variant="outlined"> Chip </v-chip>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PC line computers shops",
+  data() {
+    return {
+      name: "Message",
+      greetings: "Hellodda",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
